4eff5a3e77d3e2dd24482c43ffd50237a808712a
Marco Beijen <mbeijen1@gmail.com>
2022-08-31 14:49:58 +0200

    Change index.php to aoc executable, Add run

--- src/Base/BaseClass.php
+++ src/Base/BaseClass.php
@@ -3,13 +3,21 @@
 namespace Thecad\AdventOfCode\Base;
 
 class BaseClass {
-    protected $input = [];
+    protected array $input = [];
+    protected string $relativePath = '';
+    private string $inputFile;
 
     public function __construct() {
+        $pos = strrpos(get_class($this), '\\');
+        $this->inputFile = sprintf("%s_input.txt", substr(get_class($this), $pos + 1));
         $this->readInput();
     }
 
     private function readInput(): void {
-        // Doe iets
+        $file = fopen(sprintf("%s/inputs/%s", $this->relativePath, $this->inputFile), 'rb');
+        while(($line = fgets($file)) !== false) {
+            $this->input[] = trim($line);
+        }
+        fclose($file);
     }
 }
