7c647527f6a5e06d932aae6f8dbb0b76a55769e6
Marco Beijen <mbeijen1@gmail.com>
2022-09-16 20:37:39 +0200

    Add part 2 of day 3

--- src/Year2015/Day03.php
+++ src/Year2015/Day03.php
@@ -31,7 +31,6 @@ class Day03 extends BaseClass {
                     case 'V':
                         $y--;
                         break;
-
                     default:
                         break;
                 }
@@ -43,7 +42,52 @@ class Day03 extends BaseClass {
         return $res[1];
     }
 
-    public function partTwo(): void {
-        // Create solution
+    public function partTwo(): int {
+        $santa = new Stepper();
+        $robo = new Stepper();
+        $santa->visisted["0.0"] = 1;
+        $robo->visisted["0.0"] = 1;
+        $x = 0;
+
+        foreach ($this->input as $row) {
+            $arr = str_split($row);
+            foreach ($arr as $step) {
+                if ($x === 0) {
+                    $santa->step($step);
+                    $x = 1;
+                } else {
+                    $robo->step($step);
+                    $x = 0;
+                }
+            }
+        }
+        $total = array_unique(array_merge(array_keys($santa->visisted), array_keys($robo->visisted)));
+        return count($total);
+    }
+}
+
+class Stepper {
+    public int $x = 0, $y = 0;
+    public array $visisted = [];
+
+    public function step(string $direction) {
+        switch ($direction) {
+            case '>':
+                $this->x++;
+                break;
+            case '<':
+                $this->x--;
+                break;
+            case '^':
+                $this->y++;
+                break;
+            case 'v':
+            case 'V':
+                $this->y--;
+                break;
+            default:
+                break;
+        }
+        $this->visisted["$this->x.$this->y"] = 1;
     }
 }
