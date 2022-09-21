d2f8a9811b8b2420fe5d9c6b08e767a23eb745bb
Marco Beijen <mbeijen1@gmail.com>
2022-09-21 16:34:18 +0200

    Update part 1 and add part 2 of day 6

--- src/Year2015/Day06.php
+++ src/Year2015/Day06.php
@@ -41,19 +41,38 @@ class Day06 extends BaseClass {
     }
 
     public function partTwo(): int {
-        // Create solution
+        $this->fillGrid();
+        
+        foreach ($this->input as $row) {
+            preg_match_all("/turn on|turn off|toggle|\d*,\d*/", $row, $directions);
+            $directions = $directions[0]; // flatten array
+            $directions[1] = explode(',', $directions[1]);
+            $directions[2] = explode(',', $directions[2]);
 
-        return 0;
+            switch ($directions[0]) {
+                case "toggle":
+                    $this->toggleBrightness($directions[1], $directions[2]);
+                    break;
+                case "turn on":
+                    $this->turnOnBrightness($directions[1], $directions[2]);
+                    break;
+                case "turn off":
+                    $this->turnOffBrightness($directions[1], $directions[2]);
+                    break;
+                default:
+                    break;
+            }
+        }
+        return $this->checkBrightness();
     }
 
     private function toggle(array $start, array $end): void {
-
         for ($i = $start[0]; $i <= $end[0]; $i++) {
             for ($j = $start[1]; $j <= $end[1]; $j++) {
-                if ($this->grid[$i][$j] === 'o') {
-                    $this->grid[$i][$j] = 'x';
+                if ($this->grid[$i][$j] === '0') {
+                    $this->grid[$i][$j] = '1';
                 } else {
-                    $this->grid[$i][$j] = 'o';
+                    $this->grid[$i][$j] = '0';
                 }
             }
         }
@@ -62,7 +81,7 @@ class Day06 extends BaseClass {
     private function turnOn(array $start, array $end): void {
         for ($i = $start[0]; $i <= $end[0]; $i++) {
             for ($j = $start[1]; $j <= $end[1]; $j++) {
-                $this->grid[$i][$j] = 'o';
+                $this->grid[$i][$j] = '1';
             }
         }
     }
@@ -70,7 +89,7 @@ class Day06 extends BaseClass {
     private function turnOff(array $start, array $end): void {
         for ($i = $start[0]; $i <= $end[0]; $i++) {
             for ($j = $start[1]; $j <= $end[1]; $j++) {
-                $this->grid[$i][$j] = 'x';
+                $this->grid[$i][$j] = '0';
             }
         }
     }
@@ -79,9 +98,44 @@ class Day06 extends BaseClass {
         $total = 0;
         foreach ($this->grid as $row) {
             foreach ($row as $line) {
-                if ($line === 'o') {
+                if ($line === '1') {
                     $total++;
                 }
+            }
+        }
+        return $total;
+    }
+
+    private function toggleBrightness(array $start, array $end): void {
+        for ($i = $start[0]; $i <= $end[0]; $i++) {
+            for ($j = $start[1]; $j <= $end[1]; $j++) {
+                $this->grid[$i][$j] += 2;
+            }
+        }
+    }
+
+    private function turnOnBrightness(array $start, array $end): void {
+        for ($i = $start[0]; $i <= $end[0]; $i++) {
+            for ($j = $start[1]; $j <= $end[1]; $j++) {
+                ++$this->grid[$i][$j];
+            }
+        }
+    }
+
+    private function turnOffBrightness(array $start, array $end): void {
+        for ($i = $start[0]; $i <= $end[0]; $i++) {
+            for ($j = $start[1]; $j <= $end[1]; $j++) {
+                if ($this->grid[$i][$j] > 0)
+                --$this->grid[$i][$j];
+            }
+        }
+    }
+
+    private function checkBrightness(): int {
+        $total = 0;
+        foreach ($this->grid as $row) {
+            foreach ($row as $line) {
+                $total += $line;
             }
         }
         return $total;
@@ -90,7 +144,7 @@ class Day06 extends BaseClass {
     private function fillGrid(): void {
         for ($i = 0; $i < 1000; $i++) {
             for ($j = 0; $j < 1000; $j++) {
-                $this->grid[$i][$j] = 'x';
+                $this->grid[$i][$j] = '0';
             }
         }
     }
