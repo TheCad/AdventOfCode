92dda8906642ceea49a2a4cb1cdc3da8fe6a88a7
Marco Beijen <mbeijen1@gmail.com>
2022-11-01 11:09:24 +0100

    Add Year 2016 and Day 2

---
+++ src/Year2016/Day02.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Thecad\AdventOfCode\Year2016;
+
+use Thecad\AdventOfCode\Base\BaseClass;
+
+class Day02 extends BaseClass {
+    public function __construct() {
+        $this->relativePath = __DIR__;
+        parent::__construct();
+    }
+
+    public function partOne(): int {
+        // Create solution
+
+        return 0;
+    }
+
+    public function partTwo(): int {
+        // Create solution
+
+        return 0;
+    }
+}
