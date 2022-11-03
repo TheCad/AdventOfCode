d2df771b04bfaa052afef771857c089bb2c49bb7
Marco Beijen <mbeijen1@gmail.com>
2022-11-03 16:31:28 +0100

    Add day 3 part 1 and 2

---
+++ tests/Year2016/Day03Test.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Thecad\AdventOfCode\Tests\Year2016;
+
+use PHPUnit\Framework\TestCase;
+use Thecad\AdventOfCode\Year2016\Day03;
+
+class Day03Test extends TestCase {
+
+    private Day03 $sut;
+    protected function setUp(): void {
+        parent::setUp();
+        $this->sut = new Day03();
+    }
+
+    public function testPart1(): void {
+        self::assertTrue(true);
+    }
+
+    public function testPart2(): void {
+        self::assertTrue(true);
+    }
+}
