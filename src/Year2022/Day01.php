5e87af27ed7e775aeac3aaa4e70bde7aea557a84
Marco Beijen <mbeijen1@gmail.com>
2022-12-01 10:16:47 +0100

    Extract method

--- src/Year2022/Day01.php
+++ src/Year2022/Day01.php
@@ -13,6 +13,20 @@ class Day01 extends BaseClass {
 
     public function partOne(): int {
         // Create solution
+        $this->totals = [];
+        $this->countCals();
+        return max($this->totals);
+    }
+
+    public function partTwo(): int {
+        // Create solution
+        $this->totals = [];
+        $this->countCals();
+        rsort($this->totals);
+        return $this->totals[0] + $this->totals[1] + $this->totals[2];
+    }
+
+    private function countCals() {
         $x = 0;
         foreach ($this->input as $line) {
             if ($line === '') {
@@ -22,25 +36,5 @@ class Day01 extends BaseClass {
             $x += (int)$line;
         }
         array_push($this->totals, $x);
-
-        return max($this->totals);
-    }
-
-    public function partTwo(): int {
-        // Create solution
-        $this->totals = [];
-        $x = 0;
-        foreach($this->input as $line) {
-            if ($line === '') {
-                array_push($this->totals, $x);
-                $x = 0;
-            }
-            $x += (int)$line;
-        }
-        array_push($this->totals, $x);
-        rsort($this->totals);
-        $sum = $this->totals[0] + $this->totals[1] + $this->totals[2];
-
-        return $sum;
     }
 }
