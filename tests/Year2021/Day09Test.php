c7cd86406387570ea63ad39443a68c219e1ac027
Marco Beijen <mbeijen1@gmail.com>
2022-12-01 17:03:38 +0100

    add day 9 of 2021

---
+++ tests/Year2021/Day09Test.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Thecad\AdventOfCode\Tests\Year2021;
+
+use PHPUnit\Framework\TestCase;
+use Thecad\AdventOfCode\Year2021\Day09;
+
+class Day09Test extends TestCase {
+
+    private Day09 $sut;
+    protected function setUp(): void {
+        parent::setUp();
+        $this->sut = new Day09();
+    }
+
+    public function testPart1(): void {
+        self::assertTrue(true);
+    }
+
+    public function testPart2(): void {
+        self::assertTrue(true);
+    }
+}
