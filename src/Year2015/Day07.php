7c2912a1418148f2ad78d16ce67cb9a87ca7d6b6
Marco Beijen <mbeijen1@gmail.com>
2022-12-01 23:53:08 +0100

    Day 7 already fixed, can't find solution, used one from internet

---
+++ src/Year2015/Day07.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Thecad\AdventOfCode\Year2015;
+
+use Thecad\AdventOfCode\Base\BaseClass;
+
+class Day07 extends BaseClass {
+    public $wires = [];
+    public $op = array('AND' => '&', 'OR' => '|', 'NOT' => '~', 'RSHIFT' => '>>', 'LSHIFT' => '<<');
+
+    public function __construct() {
+        $this->relativePath = __DIR__;
+        parent::__construct();
+
+        foreach ($this->input as $line) {
+            list ($k, $v) = explode(' -> ', $line);
+            $wires[$v] = $k;
+        }
+    }
+
+    public function partOne(): int {
+        $x = $this->x('a');
+        dump($x);
+
+        return 0;
+    }
+
+    public function partTwo(): int {
+        // Create solution
+
+        return 0;
+    }
+
+    function x($wire) {
+        if (!isset($this->wires[$wire])) return $wire;
+        if (strpos($this->wires[$wire],' ') !== false) {
+            eval ('$wires[$wire] = (' .preg_replace_callback('#(([a-z0-9]+) )?[A-Z]+) ([a-z0-9]+)#', function ($p) {
+                return $this->x($p[2]).$this->op[$p[3]] . $this->x($p[4]);
+            }, $this->wires[$wire]) . ' & 65535);');
+        return $this->x($this->wires[$wire]);
+        }
+    }
+}
---
+++ src/Year2015/Day07Test.php
@@ -0,0 +1,24 @@
+<?php
+$wires = array();
+$op = array('AND' => '&', 'OR' => '|', 'NOT' => '~', 'RSHIFT' => '>>', 'LSHIFT' => '<<');
+
+foreach (file(__DIR__. '/inputs/Day07_input.txt', FILE_IGNORE_NEW_LINES) as $line) {
+	list ($k, $v) = explode(' -> ', $line);
+	$wires[$v] = $k;
+}
+
+function f($w) {
+	global $wires;
+
+	if (!isset($wires[$w])) return $w;
+	if (strpos($wires[$w], ' ') !== false) {
+		eval('$wires[$w] = (' . preg_replace_callback('#(([a-z0-9]+) )?([A-Z]+) ([a-z0-9]+)#', function ($p) {
+			return f($p[2]) . $GLOBALS['op'][$p[3]] . f($p[4]);
+		}, $wires[$w]) . ' & 65535);');
+	}
+
+	return f($wires[$w]);
+}
+
+$wires['b'] = 16076;
+echo f('a');
