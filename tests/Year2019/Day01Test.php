5970cf755c0b39a4d7d6e51f6d04034e1945a064
Marco Beijen <mbeijen1@gmail.com>
2023-05-23 20:07:29 +0200

    Recreate Day 1 of Year 2019

--- tests/Year2019/Day01Test.php
+++ tests/Year2019/Day01Test.php
@@ -13,11 +13,52 @@ class Day01Test extends TestCase {
         $this->sut = new Day01();
     }
 
-    public function testPart1(): void {
-        self::assertTrue(true);
+    public function testPart1Test1(): void {
+        $exp = 2;
+        $this->sut->setTestInput(['12']);
+        $res = $this->sut->partOne();
+        self::assertEquals($exp, $res);
     }
 
-    public function testPart2(): void {
-        self::assertTrue(true);
+    public function testPart1Test2(): void {
+        $exp = 2;
+        $this->sut->setTestInput(['14']);
+        $res = $this->sut->partOne();
+        self::assertEquals($exp, $res);
+    }
+
+    public function testPart1Test3(): void {
+        $exp = 654;
+        $this->sut->setTestInput(['1969']);
+        $res = $this->sut->partOne();
+        self::assertEquals($exp, $res);
+    }
+
+    public function testPart1Test4(): void {
+        $exp = 33583;
+        $this->sut->setTestInput(['100756']);
+        $res = $this->sut->partOne();
+        self::assertEquals($exp, $res);
+    }
+
+    public function testPart2Test1(): void {
+        $exp = 2;
+        $this->sut->setTestInput(['14']);
+        $res = $this->sut->partTwo();
+        self::assertEquals($exp, $res);
+    }
+
+    public function testPart2Test2(): void {
+        $exp = 966;
+        $this->sut->setTestInput(['1969']);
+        $res = $this->sut->partTwo();
+        self::assertEquals($exp, $res);
+    }
+
+    public function testPart2Test3(): void {
+        $exp = 50346;
+        $this->sut->setTestInput(['100756']);
+        $res = $this->sut->partTwo();
+        self::assertEquals($exp, $res);
     }
 }
