e6ec2ce83bd8ae252d48c115e96385813f75394f
Marco Beijen <mbeijen1@gmail.com>
2024-12-01 14:49:08 +0100

    Add timing to execution

--- src/Commands/Run.php
+++ src/Commands/Run.php
@@ -11,51 +11,70 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Dotenv\Dotenv;
 
 #[AsCommand(name: 'run', description: 'Runs the given year and day.')]
-class Run extends Command {
-    protected SymfonyStyle $io;
+class Run extends Command
+{
+  protected SymfonyStyle $io;
 
-    protected function configure() {
-        (new Dotenv())->usePutenv()->bootEnv(dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . '.env');
-        $setYear = getenv('YEAR');
-        $this
-            ->addArgument('day', InputArgument::OPTIONAL, 'What day do you want to run?', (int)date('d'))
-            ->addArgument('year', InputArgument::OPTIONAL, 'What year do you want to run?', $setYear ?: date('Y'))
-            ->addArgument('part', InputArgument::OPTIONAL, 'Which part do you want to run?', null);
+  protected function configure()
+  {
+    (new Dotenv())->usePutenv()->bootEnv(dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . '.env');
+    $setYear = getenv('YEAR');
+    $this
+      ->addArgument('day', InputArgument::OPTIONAL, 'What day do you want to run?', (int)date('d'))
+      ->addArgument('year', InputArgument::OPTIONAL, 'What year do you want to run?', $setYear ?: date('Y'))
+      ->addArgument('part', InputArgument::OPTIONAL, 'Which part do you want to run?', null);
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    $this->io = new SymfonyStyle($input, $output);
+
+    $day = sprintf("%02d", $input->getArgument('day'));
+    $year = date('Y', strtotime('1/1/' . $input->getArgument('year')));
+    $part = $input->getArgument('part');
+
+    $file = sprintf("Thecad\AdventOfCode\Year%d\Day%02d", $year, $day);
+    if (!class_exists($file)) {
+      $this->io->error("This day has not yet been created");
+      return Command::INVALID;
+    }
+    $class = new $file();
+
+    switch ($part) {
+      case 1:
+        $this->io->definitionList(
+          ['Part 1' => $this->partOne($class)],
+        );
+        break;
+      case 2:
+        $this->io->definitionList(
+          ['part 2' => $this->partTwo($class)],
+        );
+        break;
+      default:
+        $this->io->definitionList(
+          ['Part 1' => $this->partOne($class)],
+          ['part 2' => $this->partTwo($class)],
+        );
+        break;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output) {
-        $this->io = new SymfonyStyle($input, $output);
+    return Command::SUCCESS;
+  }
 
-        $day = sprintf("%02d", $input->getArgument('day'));
-        $year = date('Y', strtotime('1/1/'.$input->getArgument('year')));
-        $part = $input->getArgument('part');
+  private function partOne($class)
+  {
+    $start = microtime(true);
+    $part = $class->partOne();
+    $end = microtime(true);
+    return sprintf("%d, took %f ms", $part, ($end - $start));
+  }
 
-        $file = sprintf("Thecad\AdventOfCode\Year%d\Day%02d", $year, $day);
-        if (!class_exists($file)) {
-            $this->io->error("This day has not yet been created");
-            return Command::INVALID;
-        }
-        $class = new $file();
-
-        switch ($part) {
-            case 1:
-                $this->io->definitionList(
-                    ['Part 1' => $class->partOne()],
-                );
-                break;
-            case 2:
-                $this->io->definitionList(
-                    ['Part 2' => $class->partTwo()]
-                );
-                break;
-            default:
-                $this->io->definitionList(
-                    ['Part 1' => $class->partOne()],
-                    ['Part 2' => $class->partTwo()]
-                );
-                break;
-        }
-
-        return Command::SUCCESS;
-    }
+  private function partTwo($class)
+  {
+    $start = microtime(true);
+    $part = $class->partTwo();
+    $end = microtime(true);
+    return sprintf("%d, took %f ms", $part, ($end - $start));
+  }
 }
