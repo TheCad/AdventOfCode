28f05c9273363a2cc958d4a7b536aac4144b6c2e
Marco Beijen <mbeijen1@gmail.com>
2024-12-02 23:41:25 +0100

    Update part 1

--- src/Year2024/Day02.php
+++ src/Year2024/Day02.php
@@ -16,36 +16,11 @@ class Day02 extends BaseClass
   {
     $total = 0;
     foreach ($this->input as $row) {
-      if ($this->isSafe($row)) $total++;
+      $ex = explode(' ', $row);
+      if ($this->isValid($ex)) $total++;
     }
 
     return $total;
-  }
-
-  private function isSafe($row): bool
-  {
-    $safe = true;
-    $ex = explode(' ', $row);
-
-    if ($ex[0] != $ex[1]) {
-      $isAscending = $ex[0] < $ex[1];
-      for ($i = 0; $i < count($ex) - 1; $i++) {
-        if (($isAscending && $ex[$i] >= $ex[$i + 1]) || (!$isAscending && $ex[$i] <= $ex[$i + 1])) {
-          $safe = false;
-          break;
-        }
-      }
-    } else {
-      $safe = false;
-    }
-
-    for ($i = 0; $i < count($ex) - 1; $i++) {
-      if (abs($ex[$i] - $ex[$i + 1]) < 1 || abs($ex[$i] - $ex[$i + 1]) > 3) {
-        $safe = false;
-        break;
-      }
-    }
-    return $safe;
   }
 
   public function partTwo(): int
