0e62dd64cda4fb1e231592d98244f95180c0bb9c
Marco Beijen <mbeijen1@gmail.com>
2024-12-02 23:40:09 +0100

    Add day 2 part 2

--- src/Year2024/Day02.php
+++ src/Year2024/Day02.php
@@ -50,8 +50,57 @@ class Day02 extends BaseClass
 
   public function partTwo(): int
   {
-    // Create solution
+    $total = 0;
+    foreach ($this->input as $row) {
+      if ($this->isSafeTwo($row)) $total++;
+    }
 
-    return 0;
+    return $total;
+  }
+
+  private function isSafeTwo($row): bool
+  {
+    $ex = explode(' ', $row);
+
+    if ($this->isValid($ex)) {
+      return true;
+    }
+
+    for ($removeIndex = 0; $removeIndex < count($ex); $removeIndex++) {
+      $newEx = array_values(array_merge(array_slice($ex, 0, $removeIndex), array_slice($ex, $removeIndex + 1)));
+
+      if ($this->isValid($newEx)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private function isValid(array $arr): bool
+  {
+    if (count($arr) < 2) {
+      return false;
+    }
+
+    $isIncreasing = true;
+    $isDecreasing = true;
+
+    for ($i = 0; $i < count($arr) - 1; $i++) {
+      $diff = abs($arr[$i] - $arr[$i + 1]);
+
+      if ($diff < 1 || $diff > 3) {
+        return false;
+      }
+
+      if ($arr[$i] >= $arr[$i + 1]) {
+        $isIncreasing = false;
+      }
+      if ($arr[$i] <= $arr[$i + 1]) {
+        $isDecreasing = false;
+      }
+    }
+
+    return $isIncreasing || $isDecreasing;
   }
 }
