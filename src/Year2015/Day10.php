b641f215cbfe3b456b6d24904c69a04dbf0d2030
Marco Beijen <mbeijen1@gmail.com>
2025-07-09 23:47:30 +0200

    Add day 10

---
+++ src/Year2015/Day10.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Thecad\AdventOfCode\Year2015;
+
+use Thecad\AdventOfCode\Base\BaseClass;
+
+class Day10 extends BaseClass
+{
+    public function __construct()
+    {
+        $this->relativePath = __DIR__;
+        parent::__construct();
+    }
+
+    public function partOne(): string
+    {
+        $string = $this->input[0];
+        $loopAmount = 40;
+        $endStr = '';
+        for ($iteration = 0; $iteration < $loopAmount; $iteration++) {
+            if (strlen($string) === 1) {
+                return '1'.$string;
+            }
+
+            $endStr = '';
+            $count = 1;
+
+            for ($i = 0; $i < strlen($string); $i++) {
+                if (isset($string[$i+1]) && $string[$i] === $string[$i + 1]) {
+                    $count++;
+                } else {
+                    $endStr .= $count . $string[$i];
+                    $count = 1;
+                }
+            }
+            $string = $endStr;
+        }
+        return strlen($string);
+    }
+
+    public function partTwo(): int
+    {
+        $string = $this->input[0];
+        $loopAmount = 50;
+        $endStr = '';
+        for ($iteration = 0; $iteration < $loopAmount; $iteration++) {
+            if (strlen($string) === 1) {
+                return '1'.$string;
+            }
+
+            $endStr = '';
+            $count = 1;
+
+            for ($i = 0; $i < strlen($string); $i++) {
+                if (isset($string[$i+1]) && $string[$i] === $string[$i + 1]) {
+                    $count++;
+                } else {
+                    $endStr .= $count . $string[$i];
+                    $count = 1;
+                }
+            }
+            $string = $endStr;
+        }
+        return strlen($string);
+    }
+}
+
---
+++ tests/Year2015/Day10Test.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Thecad\AdventOfCode\Tests\Year2015;
+
+use PHPUnit\Framework\TestCase;
+use Thecad\AdventOfCode\Year2015\Day10;
+
+class Day10Test extends TestCase
+{
+
+    private Day10 $sut;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->sut = new Day10();
+    }
+
+    public function testOne(): void
+    {
+        $this->sut->setTestInput(['1']);
+        $actual = $this->sut->partOne();
+        $exp = '11';
+        self::assertEquals($exp, $actual);
+    }
+
+    public function testSecond(): void
+    {
+        $this->sut->setTestInput(['11']);
+        $actual = $this->sut->partOne();
+        $exp = '21';
+        self::assertEquals($exp, $actual);
+    }
+
+    public function testThird(): void
+    {
+        $this->sut->setTestInput(['21']);
+        $actual = $this->sut->partOne();
+        $exp = '1211';
+        self::assertEquals($exp, $actual);
+    }
+
+    public function testForth(): void
+    {
+        $this->sut->setTestInput(['1211']);
+        $actual = $this->sut->partOne();
+        $exp = '111221';
+        self::assertEquals($exp, $actual);
+    }
+
+    public function testFifth(): void
+    {
+        $this->sut->setTestInput(['111221']);
+        $actual = $this->sut->partOne();
+        $exp = '312211';
+        self::assertEquals($exp, $actual);
+    }
+
+    public function testPart2(): void
+    {
+        $this->sut->partTwo();
+        self::assertTrue(true);
+    }
+}
+
