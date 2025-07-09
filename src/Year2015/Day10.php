ce0902917df251f674e94a3505d41036dff48f11
Marco Beijen <mbeijen1@gmail.com>
2025-07-09 23:48:57 +0200

    Extract function

--- src/Year2015/Day10.php
+++ src/Year2015/Day10.php
@@ -16,43 +16,34 @@ class Day10 extends BaseClass
     {
         $string = $this->input[0];
         $loopAmount = 40;
-        $endStr = '';
-        for ($iteration = 0; $iteration < $loopAmount; $iteration++) {
-            if (strlen($string) === 1) {
-                return '1'.$string;
-            }
-
-            $endStr = '';
-            $count = 1;
-
-            for ($i = 0; $i < strlen($string); $i++) {
-                if (isset($string[$i+1]) && $string[$i] === $string[$i + 1]) {
-                    $count++;
-                } else {
-                    $endStr .= $count . $string[$i];
-                    $count = 1;
-                }
-            }
-            $string = $endStr;
-        }
-        return strlen($string);
+        return $this->lookAndSay($loopAmount, $string);
     }
 
     public function partTwo(): int
     {
         $string = $this->input[0];
         $loopAmount = 50;
+        return $this->lookAndSay($loopAmount, $string);
+    }
+
+    /**
+     * @param int $loopAmount
+     * @param string $string
+     * @return string
+     */
+    public function lookAndSay(int $loopAmount, string $string): string
+    {
         $endStr = '';
         for ($iteration = 0; $iteration < $loopAmount; $iteration++) {
             if (strlen($string) === 1) {
-                return '1'.$string;
+                return '1' . $string;
             }
 
             $endStr = '';
             $count = 1;
 
             for ($i = 0; $i < strlen($string); $i++) {
-                if (isset($string[$i+1]) && $string[$i] === $string[$i + 1]) {
+                if (isset($string[$i + 1]) && $string[$i] === $string[$i + 1]) {
                     $count++;
                 } else {
                     $endStr .= $count . $string[$i];
